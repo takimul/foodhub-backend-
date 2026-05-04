09f76f4af421c1e2e6945280900d7f8cff33447d
takimul <takimul3@gmail.com>
2026-05-04 16:09:12 +0600

    deployment fix

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth", toNodeHandler(auth));
