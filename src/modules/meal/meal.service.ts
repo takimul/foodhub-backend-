d761c24bb710ad61d0867b3ded4582e885ea4da2
takimul <takimul3@gmail.com>
2026-05-09 16:15:23 +0600

    reviews include user name and image

--- src/modules/meal/meal.service.ts
+++ src/modules/meal/meal.service.ts
@@ -109,8 +109,14 @@ export const MealService = {
           },
 
           reviews: {
-            select: {
-              rating: true,
+            include: {
+              customer: {
+                select: {
+                  id: true,
+                  name: true,
+                  image: true,
+                },
+              },
             },
           },
 
